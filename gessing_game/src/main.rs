70e27e99a9bfadbf9ae2b0ad593d56c700b8dda8
niikun <niikun0209@gmail.com>
2024-08-04 09:22:41 +0900

    gess_game.rs

--- gessing_game/src/main.rs
+++ gessing_game/src/main.rs
@@ -1,14 +1,38 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
-fn main() {
+
+fn main(){
     println!("Guess the number!");
-    println!("Please input your guess.");
 
-    let mut guess = String::new();
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let mut counter = 0;
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    println!("The scret_number is : {}",secret_number);
 
-    println!("You guessed: {}", guess);
+    loop{
+
+        counter += 1;
+
+        let mut guess = String::new();
+        println!("Challenge {} . Enter a number :",counter);
+        io::stdin().read_line(&mut guess).expect("Failed to read line");
+
+        let guess:u32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("You guessed: {}" , guess);
+
+        match guess.cmp(&secret_number){
+            Ordering::Less => println!("Too small !"),
+            Ordering::Greater => println!("Too big !"),
+            Ordering::Equal => {
+                println!("You win !");
+                break;
+            }
+        }
+    }
+
 }
