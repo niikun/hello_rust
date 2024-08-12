e6fa7559ac2d7fd3709a216d6e76cbe528682e02
niikun <niikun0209@gmail.com>
2024-08-12 14:55:03 +0900

    box pointer

--- rust_programming_introduction/rust_lesson/src/stack_heap.rs
+++ rust_programming_introduction/rust_lesson/src/stack_heap.rs
@@ -1,3 +1,8 @@
+enum List{
+    Node(i32, Box<List>),
+    Nil
+}
+
 pub fn run(){
     // // println!("this is stack_heap module");
     // let a1 :[u8; 5000000] = [0; 5000000];
@@ -14,5 +19,19 @@ pub fn run(){
     println!("Heap Address is {:?}",v1.as_ptr());
     println!("Heap length is :{}",v1.len());
     println!("Heap capacity is :{}",v1.capacity());
+
+    let t1:(i64,String) = (10,String::from("hello!"));
+    println!("Stack Address of t1 is :{:p}",&t1);
+    println!("Heap address of t1,1 is :{:?}",t1.1.as_ptr());
+    println!("Heap length of t1.1 is :{}",t1.1.len());
+    println!("Heap capacity of t1.1 is :{}",t1.1.capacity());
+
+    let mut b1 = Box::new(t1);
+    (*b1).1 += " world";
+    println!("{}{}",b1.0, b1.1);
+    println!("Stack asdress of b1 is :{:p}",&b1);
+    println!("Heap address of bi.1 is :{:?}",b1.1.as_ptr());
+    println!("Heap length of b1.1 is :{}",b1.1.len());
+    println!("Heap capacity of b1.1 is :{}",b1.1.capacity());
 
 }
